a53304efa23797aae66cbb36b5cd69be20a2bb2a
Kennedy <nofuequequise@gmail.com>
2022-08-09 11:58:46 -0400

    Cap-5

---
+++ src/scope/re.js
@@ -0,0 +1,32 @@
+var firstName; //undefined
+firstName = 'Kennedy',
+console.log(firstName);
+
+var lastName = 'Almonte'; //Declarar / Asignar
+lastName = 'Polanco';// Reasignar
+console.log(lastName);
+
+var secondName = 'Kennedy';//Declarando /Asignando
+var secondName = 'Ana';//reasignando
+console.log(secondName);
+
+//Let
+let fruit = 'Apple';//Declarar / Asignar
+fruit = 'Kiwi';//reasignando
+console.log(fruit)
+
+let fruit = 'Banana';
+console.log(fruit);//No se puede redeclarar
+
+//Const 
+const animal = 'Dog';
+//animal = 'Cat';
+const animal = 'Snake';
+console.log(animal);
+
+const vehicles = [];
+vehicles.push('Honda');
+console.log(vehicles);
+
+vehicles.pop();
+console.log(vehicles);
